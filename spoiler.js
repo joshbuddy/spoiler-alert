8e8f5f37826fca48f90abf9796e1199ff4c8b2ff
Josh Hull <jhull@leapmotion.com>
2013-02-23 20:59:16 -0800

    just use svg

--- spoiler.js
+++ spoiler.js
@@ -26,11 +26,8 @@
 
       var applyBlur = function() {
         var radius = maxBlur - step
-        if (browser.mozilla) {
+        if (browser.mozilla || browser.msie) {
           var filterValue = radius > 0 ? 'url(./blur.svg#blur'+radius+')' : ''
-          $spoiler.css('filter', filterValue)
-        } else if (browser.msie) {
-          var filterValue = radius > 0 ? 'progid:DXImageTransform.Microsoft.Blur(pixelRadius='+radius+')' : ''
           $spoiler.css('filter', filterValue)
         } else {
           var filterValue = radius > 0 ? 'blur('+radius+'px)' : ''
