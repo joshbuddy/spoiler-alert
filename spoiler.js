bd2622bf58d7f901d1179160e930d82a9bbb3a27
Joshua Hull <joshbuddy@gmail.com>
2016-03-14 00:23:00 -0700

    Use transitions, remove jquery and ad hoc animations

--- spoiler.js
+++ spoiler.js
@@ -1,82 +1,84 @@
-window.spoilerAlert = function(selector, opts) {
-  var elements = document.querySelectorAll(selector);
-  var defaults = {
-    max: 4,
-    partial: 2,
-    hintText: 'Click to reveal completely'
-  };
+(function() {
+  if (typeof Object.assign != 'function') {
+    (function () {
+      Object.assign = function (target) {
+        'use strict';
+        if (target === undefined || target === null) {
+          throw new TypeError('Cannot convert undefined or null to object');
+        }
 
-  elements.forEach(function(el) {
-    el.attributes.setNamedItem('data-spoiler-state', 'shrouded');
+        var output = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+          var source = arguments[index];
+          if (source !== undefined && source !== null) {
+            for (var nextKey in source) {
+              if (source.hasOwnProperty(nextKey)) {
+                output[nextKey] = source[nextKey];
+              }
+            }
+          }
+        }
+        return output;
+      };
+    })();
+  }
 
-    var animationTimer = null
-    var currentBlur = maxBlur
+  window.spoilerAlert = function(selector, opts) {
+    var elements = document.querySelectorAll(selector);
+    var defaults = {
+      max: 4,
+      partial: 2,
+      hintText: 'Click to reveal completely'
+    };
 
-    var cancelTimer = function() {
-      if (animationTimer) {
-        clearTimeout(animationTimer)
-        animationTimer = null
+    opts = Object.assign(defaults, opts || {});
+
+    var maxBlur = opts.max;
+    var partialBlur = opts.partial;
+    var hintText = opts.hintText;
+
+    var processElement = function(index) {
+      var el = elements[index];
+      el['data-spoiler-state'] = 'shrouded';
+
+      el.style.webkitTransition = '-webkit-filter 250ms';
+      el.style.transition = 'filter 250ms';
+
+      var applyBlur = function(radius) {
+        el.style.filter = 'blur('+radius+'px)';
+        el.style.webkitFilter = 'blur('+radius+'px)';
       }
+
+      applyBlur(maxBlur);
+
+      el.addEventListener('mouseover', function(e) {
+        el.style.pointer = 'Cursor';
+        el.title = hintText;
+        if (el['data-spoiler-state'] === 'shrouded') applyBlur(partialBlur);
+      })
+
+      el.addEventListener('mouseout', function(e) {
+        el.title = hintText;
+        if (el['data-spoiler-state'] === 'shrouded') applyBlur(maxBlur);
+      })
+
+      el.addEventListener('click', function(e) {
+        switch(el['data-spoiler-state']) {
+          case 'shrouded':
+            el['data-spoiler-state'] = 'revealed';
+            el.title = '';
+            el.style.cursor = 'auto';
+            applyBlur(0);
+            break;
+          default:
+            el['data-spoiler-state'] = 'shrouded';
+            el.title = hintText;
+            el.style.cursor = 'pointer';
+            applyBlur(maxBlur);
+        }
+      })
     }
 
-    var applyBlur = function(radius) {
-      currentBlur = radius
-      if (browser.msie) {
-        var filterValue = "progid:DXImageTransform.Microsoft.Blur(pixelradius="+radius+")"
-        $spoiler.css('filter', filterValue)
-      } else if (browser.mozilla) {
-        var filterValue = radius > 0 ? "url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'>" +
-          "<filter id='blur'><feGaussianBlur stdDeviation='" + radius + "' /></filter></svg>#blur\")" : ''
-        $spoiler.css('filter', filterValue)
-      } else {
-        var filterValue = radius > 0 ? 'blur('+radius+'px)' : ''
-        $spoiler.css('filter', filterValue)
-          .css('-webkit-filter', filterValue)
-          .css('-moz-filter', filterValue)
-          .css('-o-filter', filterValue)
-          .css('-ms-filter', filterValue)
-      }
-    }
-
-    var performBlur = function(targetBlur, direction) {
-      cancelTimer()
-      if (currentBlur != targetBlur) {
-        applyBlur(currentBlur + direction)
-        animationTimer = setTimeout(function() { performBlur(targetBlur, direction) }, 10)
-      }
-    }
-
-    // Does the user have IE 9 or less?
-    var ieLessThanTen = function() {
-      // This conditional check will return true if browser supports CANVAS
-      // IE9 and under do not support CANVAS and this function is only ever
-      // called by the IE checking function anyway
-      return !document.createElement('canvas').getContext
-    }
-
-    applyBlur(currentBlur)
-
-    $spoiler.on('mouseover', function(e) {
-      $spoiler.css('cursor', 'pointer')
-        .attr('title', hintText)
-      if ($spoiler.data('spoiler-state') == 'shrouded') performBlur(partialBlur, -1)
-    })
-    $spoiler.on('mouseout', function(e) {
-      if ($spoiler.data('spoiler-state') == 'shrouded') performBlur(maxBlur, 1)
-    })
-    $spoiler.on('click', function(e) {
-      if ($spoiler.data('spoiler-state') == 'shrouded') {
-        $spoiler.data('spoiler-state', 'revealed')
-          .attr('title', '')
-          .css('cursor', 'auto')
-        performBlur(0, -1)
-      } else {
-        $spoiler.data('spoiler-state', 'shrouded')
-          .attr('title', hintText)
-          .css('cursor', 'pointer')
-        performBlur(partialBlur, 1)
-      }
-    })
-
-  });
-}
+    for (var i = 0; i !== elements.length; i++) processElement(i);
+  }
+})();
